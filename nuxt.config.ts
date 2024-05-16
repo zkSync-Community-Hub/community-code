e858f98beeae8728b2280bd5227c2b4015e1f57c
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-16 16:17:58 -0400

    fix: app name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo'],
   site: {
-    name: 'Code Community',
+    name: 'Community Code',
     url: process.env.NUXT_SITE_ENV ? 'https://staging-code.zksync.io' : 'https://code.zksync.io',
   },
   runtimeConfig: {
