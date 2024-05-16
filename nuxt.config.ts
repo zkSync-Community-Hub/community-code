86b32316c01b826e2ab4842951d0a8659bb4a4d2
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-16 14:56:11 -0400

    chore: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo'],
+  site: {
+    name: 'Code Community',
+    url: process.env.NUXT_SITE_ENV ? 'https://staging-code.zksync.io' : 'https://code.zksync.io',
+  },
   runtimeConfig: {
     public: {
       app: 'code',
