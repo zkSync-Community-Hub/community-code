9e025318fb10e9a185ae89c6924097c8f4e396a5
Sabrina <sf@matterlabs.dev>
2024-06-27 17:19:04 -0400

    feat: switch to use docs-nuxt-template npm pkg (#30)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
+  extends: [['@matterlabs/docs-nuxt-template']],
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo', 'nuxt-gtag'],
   site: {
     name: 'Community Code',
