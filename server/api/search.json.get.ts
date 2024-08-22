0125ad7dd9e36b204d2bc3db9b575622471a8985
Sabrina <sf@matterlabs.dev>
2024-08-22 18:04:21 -0400

    fix: trailing slash (#44)

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -2,6 +2,6 @@ import { serverQueryContent } from '#content/server';
 
 export default eventHandler(async (event) => {
   return serverQueryContent(event)
-    .where({ _type: 'markdown', navigation: { $ne: false } })
+    .where({ _type: 'markdown', navigation: { $ne: false }, _partial: false })
     .find();
 });
