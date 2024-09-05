822a018cbfbc872b49bfc1cc03d16918cc391c30
Sabrina <sf@matterlabs.dev>
2024-09-05 17:05:44 -0400

    fix: header links (#66)
    # Description
    
    Update docs-nuxt-template to fix missing main nav links.

--- server/api/search.json.get.ts
+++
@@ -1,7 +0,0 @@
-import { serverQueryContent } from '#content/server';
-
-export default eventHandler(async (event) => {
-  return serverQueryContent(event)
-    .where({ _type: 'markdown', navigation: { $ne: false }, _partial: false })
-    .find();
-});
