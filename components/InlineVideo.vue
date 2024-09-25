f5ee6bcc4ebe084f57371400d03e6cdb82eae1b5
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-09-25 10:43:02 -0600

    fix: use video component (#81)
    adds a video component

---
+++ components/InlineVideo.vue
@@ -0,0 +1,13 @@
+<template>
+  <video
+    style="clip-path: inset(2px 2px)"
+    width="100%"
+    height="100%"
+    controls
+  >
+    <source
+      src="~/assets/webauthn-demo.mp4"
+      type="video/mp4"
+    />
+  </video>
+</template>
