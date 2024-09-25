81c8c70f3fb1a47d1a7dc5263673b3946cbc09a3
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-09-25 09:03:53 -0600

    fix: use video component

---
+++ components/InlineVideo.vue
@@ -0,0 +1,20 @@
+<template>
+  <video
+    style="clip-path: inset(2px 2px)"
+    width="100%"
+    height="100%"
+    controls
+  >
+    <source
+      v-if="videoPath"
+      :src="videoPath"
+      type="video/mp4"
+    />
+  </video>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  videoPath: string;
+}>();
+</script>
