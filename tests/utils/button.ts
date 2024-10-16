10d02277791f33f8fe297601328581962af1d6e7
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-10-16 11:45:28 -0600

    chore: cleanup

--- tests/utils/button.ts
+++ tests/utils/button.ts
@@ -1,10 +1,7 @@
 import type { Page } from '@playwright/test';
 
 export async function clickButtonByText(page: Page, selector: string | RegExp) {
-  const buttons = page.locator('button');
-  const buttonByText = buttons.getByText(selector);
-  await buttonByText.click();
-  await page.waitForTimeout(1000);
+  await page.locator('button').getByText(selector).click();
 }
 
 export async function clickCopyButton(page: Page, id: string) {
