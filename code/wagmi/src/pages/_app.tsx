eda364493103853824337ea5be07d7d76d80a8ad
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-11-15 17:09:53 -0700

    chore: remove styling

--- code/wagmi/src/pages/_app.tsx
+++ code/wagmi/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
--- tests/wagmi.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-import { setupAndRunTest } from './utils/runTest';
-
-test('Using Wagmi with ZKsync', async ({ page, context }) => {
-  await setupAndRunTest(page, context, ['/guide-wagmi'], 'wagmi');
-});
