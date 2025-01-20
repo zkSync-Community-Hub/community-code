82c7b8094ce086141433a3dcd318b85a315d170a
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-01-20 12:49:27 -0700

    fix: other tests

--- tests/configs/daily-spend-limit.ts
+++ tests/configs/daily-spend-limit.ts
@@ -14,11 +14,11 @@ export const steps: IStepConfig = {
   },
   'delete-template-files': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'install-deps': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'wait-for-install': {
     action: 'wait',
@@ -26,7 +26,7 @@ export const steps: IStepConfig = {
   },
   'hardhat-config': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/hardhat.config.ts',
+    filepath: 'tests-output/daily-spend-limit/hardhat.config.ts',
   },
   'start-local-node': {
     action: 'runCommand',
@@ -34,14 +34,14 @@ export const steps: IStepConfig = {
   },
   'deploy-to-local-node': {
     action: 'modifyFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/hardhat.config.ts',
-    atLine: 8,
-    removeLines: [8],
+    filepath: 'tests-output/daily-spend-limit/hardhat.config.ts',
+    atLine: 9,
+    removeLines: [9],
     useSetData: '  defaultNetwork: "anvilZKsync",',
   },
   'add-spend-limit-file': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'open-spend-limit-code-panel': {
     action: 'clickButtonByText',
@@ -49,11 +49,11 @@ export const steps: IStepConfig = {
   },
   'spend-limit-contract': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/contracts/SpendLimit.sol',
+    filepath: 'tests-output/daily-spend-limit/contracts/SpendLimit.sol',
   },
   'account-contract-file': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'open-account-code-panel': {
     action: 'clickButtonByText',
@@ -61,39 +61,39 @@ export const steps: IStepConfig = {
   },
   'add-account-contract': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/contracts/Account.sol',
+    filepath: 'tests-output/daily-spend-limit/contracts/Account.sol',
   },
   'aa-factory-file': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'aa-factory-contract': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/contracts/AAFactory.sol',
+    filepath: 'tests-output/daily-spend-limit/contracts/AAFactory.sol',
   },
   compile: {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'create-deploy-factory-script-file': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'modify-env-file': {
     action: 'modifyFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/.env',
+    filepath: 'tests-output/daily-spend-limit/.env',
     useSetData: 'WALLET_PRIVATE_KEY=0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110',
     atLine: 1,
     removeLines: [1],
   },
   'deploy-factory-script': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/deploy/deploy.ts',
+    filepath: 'tests-output/daily-spend-limit/deploy/deploy.ts',
   },
   'run-deploy-script': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
-    saveOutput: 'tests-output/custom-spendlimit-tutorial/deploy-outputs.txt',
+    commandFolder: 'tests-output/daily-spend-limit',
+    saveOutput: 'tests-output/daily-spend-limit/deploy-outputs.txt',
   },
   'wait-for-script': {
     action: 'wait',
@@ -101,48 +101,48 @@ export const steps: IStepConfig = {
   },
   'create-set-limit-script': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'get-deployed-account-address': {
     action: 'extractDataToEnv',
-    dataFilepath: 'tests-output/custom-spendlimit-tutorial/deploy-outputs.txt',
+    dataFilepath: 'tests-output/daily-spend-limit/deploy-outputs.txt',
     regex: /(?<=SC Account deployed on address\s)0x[a-fA-F0-9]{40}(?![\s\S]*SC Account deployed on address)/,
     variableName: 'DEPLOYED_ACCOUNT_ADDRESS',
-    envFilepath: 'tests-output/custom-spendlimit-tutorial/.env',
+    envFilepath: 'tests-output/daily-spend-limit/.env',
   },
   'get-private-key': {
     action: 'extractDataToEnv',
-    dataFilepath: 'tests-output/custom-spendlimit-tutorial/deploy-outputs.txt',
+    dataFilepath: 'tests-output/daily-spend-limit/deploy-outputs.txt',
     regex: /0x[a-fA-F0-9a-zA-Z]{64,}/g,
     variableName: 'DEPLOYED_ACCOUNT_OWNER_PRIVATE_KEY',
-    envFilepath: 'tests-output/custom-spendlimit-tutorial/.env',
+    envFilepath: 'tests-output/daily-spend-limit/.env',
   },
   'set-limit-script': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/deploy/setLimit.ts',
+    filepath: 'tests-output/daily-spend-limit/deploy/setLimit.ts',
   },
   'run-set-limit-script': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
     checkForOutput: 'Account limit:  500000000000000',
   },
   'create-transfer-script': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
   },
   'add-transfer-script': {
     action: 'writeToFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/deploy/transferETH.ts',
+    filepath: 'tests-output/daily-spend-limit/deploy/transferETH.ts',
   },
   'add-receiver-account': {
     action: 'modifyFile',
-    filepath: 'tests-output/custom-spendlimit-tutorial/.env',
+    filepath: 'tests-output/daily-spend-limit/.env',
     useSetData: 'RECEIVER_ACCOUNT=0x36615Cf349d7F6344891B1e7CA7C72883F5dc049',
     atLine: 4,
   },
   'run-transfer-script': {
     action: 'runCommand',
-    commandFolder: 'tests-output/custom-spendlimit-tutorial',
+    commandFolder: 'tests-output/daily-spend-limit',
     expectError: 'execution reverted',
   },
 };
