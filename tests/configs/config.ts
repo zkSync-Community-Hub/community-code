82286ea1ccb2cc5eabf624c64b372ca92db332e2
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-01-29 13:22:16 -0700

    chore: remove game stuff

--- tests/configs/config.ts
+++ tests/configs/config.ts
@@ -4,7 +4,6 @@ import { steps as dailySpendLimitSteps } from './daily-spend-limit';
 import { steps as signingWithWebAuthnSteps } from './signing-txns-with-webauthn';
 import { steps as multisigSteps } from './native-aa-multisig';
 import { steps as frontendPaymasterSteps } from './frontend-paymaster';
-import { steps as zkGameSteps } from './zk-game';
 
 export function getConfig(tutorialName: string) {
   let steps;
@@ -26,9 +25,6 @@ export function getConfig(tutorialName: string) {
       break;
     case 'frontend-paymaster':
       steps = frontendPaymasterSteps;
-      break;
-    case 'zk-game':
-      steps = zkGameSteps;
       break;
     default:
       break;
