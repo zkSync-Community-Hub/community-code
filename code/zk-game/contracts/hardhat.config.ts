33b2d74fc4da980b99feaf40ceeb497da8639da2
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-06-03 13:06:56 -0600

    fix: config

--- code/zk-game/contracts/hardhat.config.ts
+++ code/zk-game/contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import type { HardhatUserConfig } from 'hardhat/config';
 
+import '@nomicfoundation/hardhat-chai-matchers';
 import '@matterlabs/hardhat-zksync';
 
 import dotenv from 'dotenv';
@@ -24,15 +25,31 @@ const config: HardhatUserConfig = {
       verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
       accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
     },
+    SophonMainnet: {
+      url: 'https://rpc.sophon.xyz',
+      ethNetwork: 'mainnet',
+      verifyURL: 'https://verification-explorer.sophon.xyz/contract_verification',
+      zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+    },
+    SophonTestnet: {
+      url: 'https://rpc.testnet.sophon.xyz',
+      ethNetwork: 'sepolia',
+      verifyURL: 'https://api-explorer-verify.testnet.sophon.xyz/contract_verification',
+      zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+    },
     dockerizedNode: {
       url: 'http://localhost:3050',
       ethNetwork: 'http://localhost:8545',
       zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
     },
     anvilZKsync: {
       url: 'http://127.0.0.1:8011',
-      ethNetwork: 'localhost',
+      ethNetwork: 'http://localhost:8545',
       zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
     },
     hardhat: {
       zksync: true,
@@ -41,12 +58,13 @@ const config: HardhatUserConfig = {
   zksolc: {
     version: 'latest',
     settings: {
+      codegen: 'yul',
       // find all available options in the official documentation
       // https://docs.zksync.io/build/tooling/hardhat/hardhat-zksync-solc#configuration
     },
   },
   solidity: {
-    version: '0.8.24',
+    version: '0.8.29',
   },
 };
 
--- tests/configs/native-aa-multisig.ts
+++ tests/configs/native-aa-multisig.ts
@@ -52,10 +52,8 @@ const steps: IStepConfig = {
     useSetCommand: 'touch .env',
   },
   'env-pk': {
-    action: 'modifyFile',
+    action: 'writeToFile',
     filepath: 'tests-output/native-aa-multisig/.env',
-    atLine: 1,
-    removeLines: [1],
     useSetData: 'WALLET_PRIVATE_KEY=0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110',
   },
   'make-deploy-script': {
