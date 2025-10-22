5b3069e3a10e4fb911e44739544e2254344c8da3
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-10-22 11:24:59 -0600

    Update code/interop-messages/contracts/ignition/modules/InteropTokenTestnet.ts
    Co-authored-by: Antonio <tonioufa@gmail.com>

--- code/interop-messages/contracts/ignition/modules/InteropTokenTestnet.ts
+++ code/interop-messages/contracts/ignition/modules/InteropTokenTestnet.ts
@@ -1,9 +1,9 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
 const ABSTRACT_CONTRACT_ADDRESS = '0x';
-const SOPHON_CONTRACT_ADDRESS = '0x';
-
+// make sure chain ids match the chains you chose
 const ABSTRACT_CHAIN_ID = '11124';
+const LENS_CHAIN_ID = '37111';
 const SOPHON_CHAIN_ID = '531050104';
 
 export default buildModule('InteropTokenModule', (m) => {
