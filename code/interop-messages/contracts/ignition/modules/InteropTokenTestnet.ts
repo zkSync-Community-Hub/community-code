e31088450f8099592aa03d20472bd6956729d2ee
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-10-22 12:06:33 -0600

    fix: token module

--- code/interop-messages/contracts/ignition/modules/InteropTokenTestnet.ts
+++ code/interop-messages/contracts/ignition/modules/InteropTokenTestnet.ts
@@ -1,14 +1,15 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
-const ABSTRACT_CONTRACT_ADDRESS = '0x';
+const ABSTRACT_CONTRACT_ADDRESS = '0x...';
+const SOPHON_CONTRACT_ADDRESS = '0x...';
+
 // make sure chain ids match the chains you chose
 const ABSTRACT_CHAIN_ID = '11124';
-const LENS_CHAIN_ID = '37111';
 const SOPHON_CHAIN_ID = '531050104';
 
 export default buildModule('InteropTokenModule', (m) => {
-  const approvedChainIds = [ABSTRACT_CHAIN_ID, SOPHON_CONTRACT_ADDRESS];
-  const approvedStakingContracts = [ABSTRACT_CONTRACT_ADDRESS, SOPHON_CHAIN_ID];
+  const approvedChainIds = [ABSTRACT_CHAIN_ID, SOPHON_CHAIN_ID];
+  const approvedStakingContracts = [ABSTRACT_CONTRACT_ADDRESS, SOPHON_CONTRACT_ADDRESS];
   console.log('Approved staking contracts:', approvedStakingContracts);
   const counter = m.contract('InteropToken', [approvedChainIds, approvedStakingContracts]);
 
